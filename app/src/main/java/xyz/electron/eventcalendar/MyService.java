a16603e23fd061544f15499e52442d5a6e70bbef
Electron <devversitymail@gmail.com>
2017-01-25 13:57:40 +0530

    WIP - service

--- app/src/main/java/xyz/electron/eventcalendar/MyService.java
+++ app/src/main/java/xyz/electron/eventcalendar/MyService.java
@@ -1,10 +1,13 @@
 package xyz.electron.eventcalendar;
 
 import android.app.IntentService;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -13,6 +16,7 @@ import java.io.IOException;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import xyz.electron.eventcalendar.provider.Contract;
 
 
 public class MyService extends IntentService {
@@ -86,7 +90,63 @@ public class MyService extends IntentService {
         // save about to shared pref
         Log.d("test", "onHandleIntent: " + about.toString());
 
+        JSONArray schedule = null;
+        try {
+            schedule = respo.getJSONArray("eventSchedule");
+            //delete old records
+            int mRowsDeleted = 0;
+            mRowsDeleted = getContentResolver().delete(Contract.SchEntry.CONTENT_URI,
+                        null, null);
 
-        // Parse Rest data and Load that into ContentProvider
+            // then add new records
+            JSONObject item = null;
+            for (int i = 0; i < schedule.length(); i++){
+                item = schedule.getJSONObject(i);
+
+                ContentValues mContentValues = new ContentValues();
+                Uri mNewUri;
+
+                mContentValues.put(Contract.SchEntry.COLUMN_NAME, item.toString());
+                // update each item in ContentProvider
+                mNewUri = getContentResolver().insert(Contract.SchEntry.CONTENT_URI,
+                        mContentValues);
+                Log.d("test", "onHandleIntent: New URI after insert " + mNewUri);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+//        Log.d("test", "onHandleIntent: " + schedule.toString());
+
+        JSONArray sponsors = null;
+        try {
+            sponsors = respo.getJSONArray("eventSponsors");
+
+            //delete old records
+            int mRowsDeleted = 0;
+            mRowsDeleted = getContentResolver().delete(Contract.SpoEntry.CONTENT_URI,
+                    null, null);
+
+            // add new rows
+
+            JSONObject item = null;
+            for (int i = 0; i < sponsors.length(); i++){
+                item = sponsors.getJSONObject(i);
+
+                ContentValues mContentValues = new ContentValues();
+                Uri mNewUri;
+
+                mContentValues.put(Contract.SpoEntry.COLUMN_NAME, item.toString());
+                // update each item in ContentProvider
+
+                mNewUri = getContentResolver().insert(Contract.SpoEntry.CONTENT_URI,
+                        mContentValues);
+                Log.d("test", "onHandleIntent: New URI after insert " + mNewUri);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+//        Log.d("test", "onHandleIntent: " + sponsors.toString());
+
+        // Parse data and Load that into ContentProvider done
     }
 }
