ade3f9671aa0f18799ca2fcd03806a5ae6efb8ea
Electron <devversitymail@gmail.com>
2017-04-04 01:25:17 +0530

    feat: about activity is converted to fragment

---
+++ app/src/main/java/xyz/electron/eventcalendar/AboutFragment.java
@@ -0,0 +1,31 @@
+package xyz.electron.eventcalendar;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AboutFragment extends Fragment {
+    private final String TAG = "AboutFragment";
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        Log.d(TAG, "onCreateView: About Fragment");
+        return inflater.inflate(R.layout.content_about, container, false);
+    }
+
+}
