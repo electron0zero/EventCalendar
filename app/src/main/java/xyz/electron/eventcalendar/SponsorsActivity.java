58c2605b2a7b2828eb3070f26c1dda58ee9c74f5
Electron <devversitymail@gmail.com>
2017-04-04 03:36:39 +0530

    feat: move sponsors to fragment

--- app/src/main/java/xyz/electron/eventcalendar/SponsorsActivity.java
+++ app/src/main/java/xyz/electron/eventcalendar/SponsorsActivity.java
@@ -1,39 +1,20 @@
 package xyz.electron.eventcalendar;
 
-import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import com.google.gson.Gson;
-
-import xyz.electron.eventcalendar.provider.Contract;
 
 public class SponsorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_sponsors);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-        // View adaptor thing
-        ListView listView = (ListView) findViewById(R.id.sponsorsListView);
-
-        Cursor cursor = getContentResolver().query(Contract.SpoEntry.CONTENT_URI, null, null, null, null);
-
-        SponsorsCursorAdapter sponsorsCursorAdapter = new SponsorsCursorAdapter(this, cursor);
-
-        listView.setAdapter(sponsorsCursorAdapter);
     }
 
 
---
+++ app/src/main/java/xyz/electron/eventcalendar/SponsorsFragment.java
@@ -0,0 +1,47 @@
+package xyz.electron.eventcalendar;
+
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import xyz.electron.eventcalendar.provider.Contract;
+
+public class SponsorsFragment extends Fragment {
+
+    private final String TAG = "SponsorsFragment";
+
+    Cursor cursor;
+    SponsorsCursorAdapter sponsorsCursorAdapter;
+    // Views
+    ListView listView;
+
+    public SponsorsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: Sponsors Fragment");
+
+        View rootView = inflater.inflate(R.layout.content_sponsors, container, false);
+        // View adaptor thing
+        listView = (ListView) rootView.findViewById(R.id.sponsorsListView);
+
+        cursor = getActivity().getContentResolver().query(Contract.SpoEntry.CONTENT_URI, null, null, null, null);
+        sponsorsCursorAdapter = new SponsorsCursorAdapter(getContext(), cursor);
+
+        listView.setAdapter(sponsorsCursorAdapter);
+
+        return rootView;
+    }
+
+}
