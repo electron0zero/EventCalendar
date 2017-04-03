fe8f34ab2854df19065d08e7e8083a9b35ea5a23
Electron <devversitymail@gmail.com>
2017-04-04 02:22:58 +0530

    feat: about activity is converted to use fragment

--- app/src/main/java/xyz/electron/eventcalendar/AboutActivity.java
+++ app/src/main/java/xyz/electron/eventcalendar/AboutActivity.java
@@ -1,63 +1,19 @@
 package xyz.electron.eventcalendar;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
-import com.google.gson.Gson;
 
 public class AboutActivity extends AppCompatActivity {
-
-    SharedPreferences mSettings;
-    String metadata;
-    String about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_about);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        // get Shared preference
-        // TODO: handle case when we get Nothing(null/None) in metadata and about
-        mSettings = getSharedPreferences(MyService.PREFS_NAME, 0);
-        metadata = mSettings.getString("metadata", "Not Found");
-        about = mSettings.getString("about", "Not Found");
-        // Log.d("test", metadata);
-        // Log.d("test", about);
-
-        // convert it to DataObj via GSON
-        Gson gson = new Gson();
-        DataObj.EventMetadataBean eventMetadataBean =
-                gson.fromJson(metadata, DataObj.EventMetadataBean.class);
-        DataObj.EventAboutBean eventAboutBean =
-                gson.fromJson(about, DataObj.EventAboutBean.class);
-
-        // get Views
-        ImageView event_icon = (ImageView) findViewById(R.id.event_icon);
-        ImageView event_poster = (ImageView) findViewById(R.id.event_poster);
-        TextView event_name = (TextView) findViewById(R.id.event_name);
-        TextView event_info = (TextView) findViewById(R.id.eventAbout_info);
-        TextView event_org = (TextView) findViewById(R.id.eventAbout_org);
-        TextView event_org_phone = (TextView) findViewById(R.id.eventAbout_org_phone);
-        TextView event_org_email = (TextView) findViewById(R.id.eventAbout_org_email);
-        TextView event_address = (TextView) findViewById(R.id.eventAbout_address);
-
-        // populate it
-        Glide.with(getApplicationContext()).load(eventMetadataBean.getIconUrl()).into(event_icon);
-        Glide.with(getApplicationContext()).load(eventMetadataBean.getPosterUrl()).into(event_poster);
-        event_name.setText(eventMetadataBean.getEvent_name());
-        event_info.setText(eventAboutBean.getInfo());
-        event_org.setText(eventAboutBean.getOrganiser());
-        event_org_phone.setText(eventAboutBean.getOrganiser_contact_phone());
-        event_org_email.setText(eventAboutBean.getOrganiser_contact_email());
-        event_address.setText(eventAboutBean.getAddress_of_event());
     }
 
 }
--- app/src/main/java/xyz/electron/eventcalendar/AboutFragment.java
+++ app/src/main/java/xyz/electron/eventcalendar/AboutFragment.java
@@ -1,31 +1,95 @@
 package xyz.electron.eventcalendar;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AboutFragment extends Fragment {
     private final String TAG = "AboutFragment";
+    SharedPreferences mSettings;
+    String metadata;
+    String about;
+
+    // Views
+    ImageView event_icon;
+    ImageView event_poster;
+    TextView event_name;
+    TextView event_info;
+    TextView event_org;
+    TextView event_org_phone;
+    TextView event_org_email;
+    TextView event_address;
+
 
     public AboutFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         Log.d(TAG, "onCreateView: About Fragment");
-        return inflater.inflate(R.layout.content_about, container, false);
+
+        View rootView = inflater.inflate(R.layout.content_about, container, false);
+
+        event_icon = (ImageView) rootView.findViewById(R.id.event_icon);
+        event_poster = (ImageView) rootView.findViewById(R.id.event_poster);
+        event_name = (TextView) rootView.findViewById(R.id.event_name);
+        event_info = (TextView) rootView.findViewById(R.id.eventAbout_info);
+        event_org = (TextView) rootView.findViewById(R.id.eventAbout_org);
+        event_org_phone = (TextView) rootView.findViewById(R.id.eventAbout_org_phone);
+        event_org_email = (TextView) rootView.findViewById(R.id.eventAbout_org_email);
+        event_address = (TextView) rootView.findViewById(R.id.eventAbout_address);
+
+
+        // get Shared preference
+        // TODO: handle case when we get Nothing(null/None) in metadata and about
+        mSettings = getActivity().getSharedPreferences(MyService.PREFS_NAME, 0);
+        metadata = mSettings.getString("metadata", "Not Found");
+        about = mSettings.getString("about", "Not Found");
+        // Log.d("test", metadata);
+        // Log.d("test", about);
+
+        // convert it to DataObj via GSON
+        Gson gson = new Gson();
+        DataObj.EventMetadataBean eventMetadataBean =
+                gson.fromJson(metadata, DataObj.EventMetadataBean.class);
+        DataObj.EventAboutBean eventAboutBean =
+                gson.fromJson(about, DataObj.EventAboutBean.class);
+
+        // populate it
+        Glide.with(getContext()).load(eventMetadataBean.getIconUrl()).into(event_icon);
+        Glide.with(getContext()).load(eventMetadataBean.getPosterUrl()).into(event_poster);
+        event_name.setText(eventMetadataBean.getEvent_name());
+        event_info.setText(eventAboutBean.getInfo());
+        event_org.setText(eventAboutBean.getOrganiser());
+        event_org_phone.setText(eventAboutBean.getOrganiser_contact_phone());
+        event_org_email.setText(eventAboutBean.getOrganiser_contact_email());
+        event_address.setText(eventAboutBean.getAddress_of_event());
+
+        return rootView;
     }
 
 }
